4361d3aa6dba8be35bdafb762ec9041e599e8c0f
luijulus <luijulus@gmail.com>
2024-09-20 02:24:14 +0500

    one more commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -42,10 +42,10 @@ const Header = () => {
       <div className='flex items-center'>
         <NavLink className={({ isActive }) =>
           isActive ? "text-lg  text-yellow uppercase px-2 border-r-2" : "text-md  text-link uppercase px-2 border-r-2"
-        } to={'/finanse-calc/call_back'}>Заказать обратный звонок</NavLink>
+        } to={'/call_back'}>Заказать обратный звонок</NavLink>
         <NavLink className={({ isActive }) =>
           isActive ? "text-lg  text-yellow uppercase px-2 border-r-2" : "text-md  text-link uppercase px-2 border-r-2"
-        } to={'/finanse-calc/credit_calculator'}>Калькулятор</NavLink>
+        } to={'/credit_calculator'}>Калькулятор</NavLink>
         <div className='lang flex align-middle pl-2 gap-2 relative' onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
           <p className='md:hidden uppercase text-link'>{t("abr")}</p>
           <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13 22.75C18.3848 22.75 22.75 18.3848 22.75 13C22.75 7.61522 18.3848 3.25 13 3.25C7.61522 3.25 3.25 7.61522 3.25 13C3.25 18.3848 7.61522 22.75 13 22.75Z" stroke="#7893B0" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path><path d="M3.80859 9.75H22.1914" stroke="#7893B0" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path><path d="M3.80859 16.25H22.1914" stroke="#7893B0" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path><path d="M13 22.4865C15.2437 22.4865 17.0625 18.2395 17.0625 13.0006C17.0625 7.76165 15.2437 3.51465 13 3.51465C10.7563 3.51465 8.9375 7.76165 8.9375 13.0006C8.9375 18.2395 10.7563 22.4865 13 22.4865Z" stroke="#7893B0" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path></svg>
