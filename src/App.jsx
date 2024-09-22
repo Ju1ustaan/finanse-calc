31ccc59190de3f5ac0aec9e52def983ae8db5963
luijulus <luijulus@gmail.com>
2024-09-22 22:58:02 +0500

    build correct

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,8 @@ import FeedbackForm from './pages/FeedbackForm';
 import Header from './components/Header';
 import CarFinansingPage from './pages/CarFinansingPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 import HomePage from './pages/HomePage';
 const App = () => {
-
-  const { t } = useTranslation()
-  console.log(t('lang'), 'test');
   
   return (
     <Router>
